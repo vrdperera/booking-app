226fedc14bfd16f55e770ac34e9c512ed80e6a1d
Vrd Perera <vrdperera@gmail.com>
2022-08-03 11:39:24 +0530

    add header component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
+import Header from '../../components/header/inde';
 import Navbar from '../../components/navbar';
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
+      <Header />
     </div>
   );
 }
