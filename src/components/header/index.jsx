90988dfd206dce8600d88e34c12079e5701e553b
Vrd Perera <vrdperera@gmail.com>
2022-08-03 13:46:10 +0530

    add additional styles

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,12 +4,22 @@ import Icon from './atoms/header-icon';
 export default function Header() {
   return (
     <div className="Header-Wrapper  bg-navbar-darkBlue text-white flex justify-center">
-      <div className="headerContainer container mx-auto">
-        <div className="headerList flex gap-8 mt-5 mb-28">
+      <div className="headerContainer container mx-auto mt-5 mb-28">
+        <div className="headerList flex gap-8 mb-[50px] ">
           {iconsForHeader.map(({ icon, name, active }) => (
             <Icon Icon={icon} name={name} active={active} />
           ))}
         </div>
+        <h1 className="headerTitle font-semibold text-4xl">
+          A lifetime of discounts? It's Genius.
+        </h1>
+        <p className="headerDesc my-5 mx-0">
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free Lamabooking account
+        </p>
+        <button className="login  bg-white w-36  text-black  p-3  rounded-sm font-medium text-xs leading-tight uppercase  hover:shadow-md">
+          Sign in / Register
+        </button>
       </div>
     </div>
   );
