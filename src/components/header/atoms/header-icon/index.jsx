a8448c6a6c64223876b1301fe8f1fe5d17a01f38
Vrd Perera <vrdperera@gmail.com>
2022-08-03 13:27:12 +0530

    rearrange skeleton

--- src/components/header/atoms/header-icon/index.jsx
+++ src/components/header/atoms/header-icon/index.jsx
@@ -2,7 +2,7 @@ export default function Icon({ Icon, name, active }) {
   console.log(name);
   return (
     <div
-      className={`flex justify-center items-center gap-2 cursor-pointer ${
+      className={`headerListItem flex justify-center items-center gap-2 cursor-pointer ${
         active ? 'border-2 p-3 rounded-xl' : ''
       }`}
     >
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,10 +4,12 @@ import Icon from './atoms/header-icon';
 export default function Header() {
   return (
     <div className="Header-Wrapper  bg-navbar-darkBlue text-white flex justify-center">
-      <div className="headerList container mx-auto flex gap-8 mt-5 mb-28">
-        {iconsForHeader.map(({ icon, name, active }) => (
-          <Icon Icon={icon} name={name} active={active} />
-        ))}
+      <div className="headerContainer container mx-auto">
+        <div className="headerList flex gap-8 mt-5 mb-28">
+          {iconsForHeader.map(({ icon, name, active }) => (
+            <Icon Icon={icon} name={name} active={active} />
+          ))}
+        </div>
       </div>
     </div>
   );
