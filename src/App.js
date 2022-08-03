12c0a04b13772cd364bb6c07be9eb2570bece12f
Vrd Perera <vrdperera@gmail.com>
2022-08-03 09:47:18 +0530

    add main routes and setup the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Hotel from './pages/hotel';
+import Hotels from './pages/hotels';
 
-export default App;
+export default function App() {
+  return (
+    <div className="App container m-auto bg-slate-500">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<Hotels />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </div>
+  );
+}
