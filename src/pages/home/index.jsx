c43d62d4ff3340db8a267886fda34ff2b4ce629c
Vrd Perera <vrdperera@gmail.com>
2022-08-10 11:46:10 +0530

    fix css

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,13 +11,13 @@ export default function Home() {
     <div className="">
       <Navbar />
       <Header />
-      <div className="HomeContainer container mx-auto mt-[50px] flex flex-col items-center gap-8 overflow-x-scroll ">
+      <div className="HomeContainer container mx-auto mt-[50px] flex flex-col items-center gap-8 overflow-x-scroll">
         <Featured />
         <h1 className="text-2xl font-semibold max-w-7xl w-full ">
           Browse by property type
         </h1>
 
-        <div className="max-w-7xl mx-auto ">
+        <div className="max-w-7xl mx-auto overflow-x-scroll">
           <div className="container mx-auto flex justify-between gap-5 overflow-x-scroll">
             {imgData.map(({ id, imgLink, alt }) => (
               <PropertyList key={id} imgLink={imgLink} alt={alt} />
