56c217350800ca61cd7d9a3a9990f4da3c0a6aff
Vrd Perera <vrdperera@gmail.com>
2022-08-19 21:23:46 +0530

    config all main routes

--- api/app.js
+++ api/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
 
 export const app = express();
 
 app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
