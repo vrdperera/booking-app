bff40dea8a3eeeaf56e8b2ad545d458996001f83
Vrd Perera <vrdperera@gmail.com>
2022-08-19 22:03:07 +0530

    add hotel schema model

---
+++ api/models/Hotel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const HotelSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+  },
+  cheapestPrice: {
+    type: Number,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model('Hotel', HotelSchema);
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,4 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
+// CREATE
+// UPDATE
+// DELETE
+// GET ONE
+// GET ALL
+
 export default router;
