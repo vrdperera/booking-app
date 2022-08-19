e3599ce9be05c20e15acbf038539fb25dcc881d7
Vrd Perera <vrdperera@gmail.com>
2022-08-19 17:38:29 +0530

    setup the basic server with mongodb for test

---
+++ api/server.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+
+import { app } from './app.js';
+
+const db = mongoose.connection;
+
+const DB_URI = process.env.DB_URI.replace(
+  '<password>',
+  process.env.DB_PASSWORD
+);
+
+const mongooseConnect = async () => {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log('db connection successfully established!');
+  } catch (error) {
+    throw error;
+  }
+};
+
+db.on('error', (error) =>
+  console.error('Error in MongoDb connection: ' + error)
+);
+db.on('connected', () => console.log('Data Db connected'));
+db.on('disconnected', () => console.log('MongoDB disconnected!'));
+
+app.listen(8080, () => {
+  mongooseConnect();
+  console.log('Connected to Server!');
+});
