8cfe7f5f30caa952fa1e42b73179b6fbe61be664
Vrd Perera <vrdperera@gmail.com>
2022-08-20 13:05:18 +0530

    add login function, config with jwt and cookie

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js';
 import createError from '../utils/error.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const inUserCollection = User;
 
@@ -27,4 +28,37 @@ const register = async (req, res, next) => {
   }
 };
 
-export default { register };
+const login = async (req, res, next) => {
+  const { username, password: pass } = req.body;
+
+  try {
+    const UserDoc = await inUserCollection.findOne({
+      username,
+    });
+    if (!UserDoc) return next(createError(404, 'User not found!'));
+
+    // check password correctness
+    const isPasswordCorrect = await bcrypt.compare(pass, UserDoc.password);
+    if (!isPasswordCorrect)
+      return next(createError(400, 'Wrong password or username!'));
+
+    const token = jwt.sign(
+      { id: UserDoc._id, isAdmin: UserDoc.isAdmin },
+      process.env.JWT_SIGNATURE
+    );
+
+    // removed password and isAdmin fields
+    const { password, isAdmin, ...restFields } = UserDoc._doc;
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json({
+        status: 'success',
+        ...restFields,
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { register, login };
