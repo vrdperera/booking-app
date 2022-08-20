f09033a1e4dc4bcfcbccdc0dab533cc7b117a2e1
Vrd Perera <vrdperera@gmail.com>
2022-08-20 13:21:38 +0530

    add user routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,18 @@
 import express from 'express';
+// import controllers
+import userController from '../controllers/userController.js';
+
 const router = express.Router();
+
+// CREATE
+router.route('/').post(userController.createUserDoc);
+// UPDATE
+router.route('/:id').patch(userController.updatedUserDoc);
+// DELETE
+router.route('/:id').delete(userController.deleteUserDoc);
+// GET ONE
+router.route('/:id').get(userController.getUserDoc);
+// GET ALL
+router.route('/').get(userController.getAllUserDocs);
 
 export default router;
