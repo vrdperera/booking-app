70b640175e8afa2c8400606a0394f27b29359948
Vrd Perera <vrdperera@gmail.com>
2022-08-21 18:48:32 +0530

    add authentications routes with the veryfyToken and veryfyUser

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,19 @@
 import express from 'express';
 // import controllers
 import userController from '../controllers/userController.js';
+import { veryfyToken, veryfyUser } from '../utils/veryfyToken.js';
 
 const router = express.Router();
+
+router.route('/checkauthentication').get(veryfyToken, (req, res, next) => {
+  res.send('Hello User welcome to the party!');
+});
+
+router.route('/checkuser/:id').get(veryfyUser, (req, res, next) => {
+  res.send(
+    'Hello User welcome to the party! now you can do what ever you want!'
+  );
+});
 
 // CREATE
 router.route('/').post(userController.createUserDoc);
