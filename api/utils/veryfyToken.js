19e680a8b37b066a16e95572f31f15ebb6611d2f
Vrd Perera <vrdperera@gmail.com>
2022-08-21 18:46:25 +0530

    add veryfyToken and verrufyUser functons

---
+++ api/utils/veryfyToken.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import createError from './error.js';
+
+export function veryfyToken(req, res, next) {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, 'You are not authenticated!'));
+  }
+
+  jwt.verify(token, process.env.JWT_SIGNATURE, (err, user) => {
+    if (err) return next(createError(403, 'Token is not valid!'));
+    req.user = user;
+  });
+  next();
+}
+
+export function veryfyUser(req, res, next) {
+  const cbNext = () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, 'You are not authorized!'));
+    }
+  };
+  veryfyToken(req, res, cbNext);
+}
