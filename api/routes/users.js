4dc50d5b47521f50701e9bc8706c19dc8a1eed5e
Vrd Perera <vrdperera@gmail.com>
2022-08-21 19:03:53 +0530

    add all the middlewares to the user routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,29 +1,35 @@
 import express from 'express';
 // import controllers
 import userController from '../controllers/userController.js';
-import { veryfyToken, veryfyUser } from '../utils/veryfyToken.js';
+import { veryfyAdmin, veryfyToken, veryfyUser } from '../utils/veryfyToken.js';
 
 const router = express.Router();
 
-router.route('/checkauthentication').get(veryfyToken, (req, res, next) => {
-  res.send('Hello User welcome to the party!');
-});
+// router.route('/checkauthentication').get(veryfyToken, (req, res, next) => {
+//   res.send('Hello User welcome to the party!');
+// });
 
-router.route('/checkuser/:id').get(veryfyUser, (req, res, next) => {
-  res.send(
-    'Hello User welcome to the party! now you can do what ever you want!'
-  );
-});
+// router.route('/checkuser/:id').get(veryfyUser, (req, res, next) => {
+//   res.send(
+//     'Hello User welcome to the party! now you can do what ever you want!'
+//   );
+// });
+
+// router.route('/checkadmin/:id').get(veryfyAdmin, (req, res, next) => {
+//   res.send(
+//     'Hello Admin welcome to the party! now you can do what ever you want!'
+//   );
+// });
 
 // CREATE
-router.route('/').post(userController.createUserDoc);
+router.route('/').post(veryfyUser, userController.createUserDoc);
 // UPDATE
-router.route('/:id').patch(userController.updatedUserDoc);
+router.route('/:id').patch(veryfyUser, userController.updatedUserDoc);
 // DELETE
-router.route('/:id').delete(userController.deleteUserDoc);
-// GET ONE
-router.route('/:id').get(userController.getUserDoc);
+router.route('/:id').delete(veryfyUser, userController.deleteUserDoc);
 // GET ALL
-router.route('/').get(userController.getAllUserDocs);
+router.route('/').get(veryfyAdmin, userController.getAllUserDocs);
+// // GET ONE
+// router.route('/:id').get(userController.getUserDoc);
 
 export default router;
